f6cefff9c95380aae4e782f26261fca154a97050
sameedmohsin28 <sameedmohsin22@hotmail.com>
2023-08-16 02:35:51 +0500

    Create empty controllers, create routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,23 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'public_recipes/index'
+  get 'inventories/index'
+  get 'inventories/show'
+  get 'inventories/new'
+  get 'inventories/create'
+  get 'inventories/destroy'
+  get 'recipes/index'
+  get 'recipes/show'
+  get 'recipes/new'
+  get 'recipes/create'
+  get 'recipes/destroy'
+  get 'shopping_list', to: 'shopping_lists#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :recipes, only: [:index, :new, :create, :show, :destroy]
+  resources :public_recipes, only: [:index]
+  resources :inventories, only: [:index, :new, :create, :show, :destroy]
 end
