8a3ee56c57c2294c6a4dfc390edd60a35d5e8d5c
abiola-arobieke <abiola.arobieke@gmail.com>
2023-08-16 17:10:52 +0100

    Update food model

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,4 +1,8 @@
 class Food < ApplicationRecord
   has_many :inventory_food
   has_many :recipe_food
+
+  validates :name, presence: true
+  validates :measurement_unit, presence: true
+  validates :price, numericality: true
 end
