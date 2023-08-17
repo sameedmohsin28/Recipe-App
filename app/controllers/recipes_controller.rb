f945e4c4d306259e7e8219f0fb0de85ee25a98e6
Javed-Shahbaz <javedshahbaz007@gmail.com>
2023-08-17 22:57:20 +0500

    add recipes controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,11 +1,45 @@
 class RecipesController < ApplicationController
-  def index; end
+  def index
+    @recipes = Recipe.all
+  end
 
-  def show; end
+  def public_recipes
+    @recipes = Recipe.where(public: true)
+  end
 
-  def new; end
+  def new
+    @recipe = Recipe.new
+  end
 
-  def create; end
+  def create
+    @recipe = Recipe.new(recipe_params)
+    @recipe.user_id = current_user.id
 
-  def destroy; end
+    if @recipe.save
+      flash[:notice] = 'Recipe was successfully created.'
+      redirect_to recipes_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @recipe = Recipe.find(params[:id])
+    @foods = Food.all
+  end
+
+  def edit; end
+
+  def destroy
+    @recipe = Recipe.find(params[:id])
+    @recipe.destroy
+    flash[:notice] = 'Recipe Deleted Successfully.'
+    redirect_to recipes_path
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description, :public)
+  end
 end
