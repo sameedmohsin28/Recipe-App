acf2f62bf4428a1940c50e9336c55796eec843da
abiola-arobieke <abiola.arobieke@gmail.com>
2023-08-17 09:37:50 +0100

    Modiy inventor_foods_controller.rb file

---
+++ app/controllers/inventory_foods_controller.rb
@@ -0,0 +1,22 @@
+class InventoryFoodsController < ApplicationController
+  def new
+    @inventory_food = InventoryFood.new
+    @inventory = InventoryFood.find(params[:inventory_id])
+  end
+
+  def create
+    @inventory_food = InventoryFood.new(inventory_food_params)
+    inventory_id = params[:inventory_id]
+
+    if @inventory_food.save
+      redirect_to inventory_path(inventory_id), notice: 'Food has been added.'
+    else
+      flash.now[:alert] = 'Error! Unable to add food.'
+      redirect_to inventory_path(inventory_id), notice: 'Unable to add food has.'
+    end
+  end
+
+  def inventory_food_params
+    params.require(:inventory_food).permit(:inventory_id, :quantity, :food_id)
+  end
+end
