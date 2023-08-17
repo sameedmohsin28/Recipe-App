8667fe817fbe366f08572d1b5fd0f4ebf82207ea
abiola-arobieke <abiola.arobieke@gmail.com>
2023-08-17 09:37:50 +0100

    Modiy inventor_foods_controller.rb file

--- app/controllers/inventories_controller.rb
+++ app/controllers/inventories_controller.rb
@@ -1,7 +1,10 @@
 class InventoriesController < ApplicationController
   def index; end
 
-  def show; end
+  def show
+    @inventory = Inventory.find(params[:id])
+    @inventory_foods = @inventory.inventory_foods
+  end
 
   def new; end
 
