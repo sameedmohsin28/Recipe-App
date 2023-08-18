db2013b80fc7e7aa66d482a3c010b1431d240b91
sameedmohsin28 <sameedmohsin22@hotmail.com>
2023-08-19 01:52:23 +0500

    Add homepage view, controller, and route

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "homes#index"
+  resources :homes, only: [:index]
   resources :recipes, only: [:index, :new, :create, :show, :destroy] do
     resources :recipe_foods, only: [:new, :create, :edit, :update, :destroy]
   end
