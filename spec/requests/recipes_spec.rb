c993819589032b126a239121b6fcf38765425124
Javed-Shahbaz <javedshahbaz007@gmail.com>
2023-08-20 04:05:28 +0500

    update spec

--- spec/requests/recipes_spec.rb
+++ spec/requests/recipes_spec.rb
@@ -7,11 +7,10 @@ RSpec.describe 'Recipes', type: :request do
     @user = User.create(name: 'Burger', email: 'burger@gmail.com', password: '123456',
                           password_confirmation: '123456')
 
-      login_as(@user, scope: :user)
+    login_as(@user, scope: :user)
 
-      get '/recipes'
-      follow_redirect!
-      expect(response).to have_http_status(:success)
-    end
+    get '/recipes'
+    follow_redirect!
+    expect(response).to have_http_status(:success)
   end
 end
