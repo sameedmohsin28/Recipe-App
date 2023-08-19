8de27870ff243234e2ae48b8485e1325e71aac62
sameedmohsin28 <sameedmohsin22@hotmail.com>
2023-08-20 01:41:21 +0500

    Add inventory new page

--- app/controllers/inventories_controller.rb
+++ app/controllers/inventories_controller.rb
@@ -8,9 +8,32 @@ class InventoriesController < ApplicationController
     @inventory_foods = @inventory.inventory_foods
   end
 
-  def new; end
+  def new
+    @inventory = Inventory.new
+  end
 
-  def create; end
+  def create
+    @inventory = Inventory.new(inventory_params)
+    @inventory.user_id = current_user.id
 
-  def destroy; end
+    if @inventory.save
+      flash[:notice] = 'inventory was successfully created.'
+      redirect_to inventories_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @inventory = Inventory.find(params[:id])
+    @inventory.destroy
+    flash[:notice] = 'inventory Deleted Successfully.'
+    redirect_to inventories_path
+  end
+
+  private
+
+  def inventory_params
+    params.require(:inventory).permit(:name, :preparation_time, :cooking_time, :description, :public)
+  end
 end
